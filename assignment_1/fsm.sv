36069b9961f0291902a530fac74eea232f0ede10
brucexi999 <91307696+brucexi999@users.noreply.github.com>
2022-10-03 08:30:56 -0700

    update

--- assignment_1/fsm.sv
+++ assignment_1/fsm.sv
@@ -15,124 +15,125 @@ module Fsm (rst, clk, aim, motion_score, detail_score, make_motion, check_motion
     typedef enum  {s_reset, s_make_motion, s_check_motion, s_wait_motion_sccore, s_increase_motion, s_decrease_motion, s_decode_detail, 
     s_make_detail, s_check_detail, s_wait_detail_score, s_increase_detail, s_decrease_detail, s_make_video, s_done} statetype;
 
-    statetype current_state, next_state; 
+    statetype state; 
 
-    always_ff @ (posedge clk) begin
-        if (rst) begin
-            current_state <= s_reset;
+    
+
+    always_ff @  (posedge clk) begin
+
+        if (rst) begin 
+            state <= s_reset;
         end
 
-        current_state <= next_state;
-    end
-
-    always_comb  begin
-        case (current_state) 
-            s_reset: begin
-                make_motion = 0; 
-                check_motion = 0;
-                make_detail = 0;
-                check_detail = 0;
-                make_video = 0;
-                motion_target = 'd100;
-                motion_prmt = 'd50;
-                detail_target = 'd0;
-                detail_prmt = 'd150;
-                if (~rst) begin
-                    next_state = s_make_motion;
-                end
-            end
-
-            s_make_motion: begin
-                make_motion = 1;
-                next_state = s_check_motion;
-            end
-
-            s_check_motion: begin
-                make_motion = 0;
-                check_motion = 1;
-                next_state = s_wait_motion_sccore;
-            end
-
-            s_wait_motion_sccore: begin
-                check_motion = 0;
-                if (motion_score < motion_target) begin
-                    next_state = s_increase_motion;
-                end
-                else if (motion_score > motion_target) begin
-                    next_state = s_decrease_motion;
+        else begin
+            case (state) 
+                s_reset: begin
+                    make_motion <= 0; 
+                    check_motion <= 0;
+                    make_detail <= 0;
+                    check_detail <= 0;
+                    make_video <= 0;
+                    motion_target <= 'd100;
+                    motion_prmt <= 'd50;
+                    detail_target <= 'd0;
+                    detail_prmt <= 'd150;
+                    if (~rst) begin
+                        state <= s_make_motion;
+                    end
                 end
 
-                else if (motion_score == motion_target) begin
-                    next_state = s_decode_detail;
-                end
-            end
-
-            s_increase_motion: begin
-                motion_prmt = motion_prmt + 1'b1; 
-                next_state = s_make_motion;
-            end
-
-            s_decrease_motion: begin
-                motion_prmt = motion_prmt - 1'b1; 
-                next_state = s_make_motion;
-            end
-
-            s_decode_detail: begin
-                case (aim)
-                    3'b001: detail_target = 'd30;
-                    3'b010: detail_target = 'd45;
-                    3'b100: detail_target = 'd60;
-                endcase
-                next_state = s_make_detail;
-            end
-
-            s_make_detail: begin
-                make_detail = 1;
-                next_state = s_check_detail;
-            end
-
-            s_check_detail: begin
-                make_detail = 0;
-                check_detail = 1;
-                next_state = s_wait_detail_score;
-            end
-
-            s_wait_detail_score: begin
-                check_detail = 0;
-                if (detail_score < detail_target) begin
-                    next_state = s_increase_detail;
-                end
-                else if (detail_score > detail_target) begin
-                    next_state = s_decrease_detail;
+                s_make_motion: begin
+                    make_motion <= 1;
+                    state <= s_check_motion;
                 end
 
-                else if (detail_score == detail_target) begin
-                    next_state = s_make_video;
+                s_check_motion: begin
+                    make_motion <= 0;
+                    check_motion <= 1;
+                    state <= s_wait_motion_sccore;
                 end
-            end
 
-            s_increase_detail: begin
-                detail_prmt = detail_prmt + 1'b1; 
-                next_state = s_make_detail;
-            end
+                s_wait_motion_sccore: begin
+                    check_motion <= 0;
+                    if (motion_score < motion_target) begin
+                        state <= s_increase_motion;
+                    end
+                    else if (motion_score > motion_target) begin
+                        state <= s_decrease_motion;
+                    end
 
-            s_decrease_detail: begin
-                detail_prmt = detail_prmt - 1'b1; 
-                next_state = s_make_detail;
-            end
-
-            s_make_video: begin
-                make_video = 1;
-                next_state = s_done;
-            end
-
-            s_done: begin
-                make_video = 0;
-                if (rst) begin
-                next_state = s_reset;
+                    else if (motion_score == motion_target) begin
+                        state <= s_decode_detail;
+                    end
                 end
-            end
-        endcase
+
+                s_increase_motion: begin
+                    motion_prmt <= motion_prmt + 1'b1; 
+                    state <= s_make_motion;
+                end
+
+                s_decrease_motion: begin
+                    motion_prmt <= motion_prmt - 1'b1; 
+                    state <= s_make_motion;
+                end
+
+                s_decode_detail: begin
+                    case (aim)
+                        3'b001: detail_target <= 'd30;
+                        3'b010: detail_target <= 'd45;
+                        3'b100: detail_target <= 'd60;
+                    endcase
+                    state <= s_make_detail;
+                end
+
+                s_make_detail: begin
+                    make_detail <= 1;
+                    state <= s_check_detail;
+                end
+
+                s_check_detail: begin
+                    make_detail <= 0;
+                    check_detail <= 1;
+                    state <= s_wait_detail_score;
+                end
+
+                s_wait_detail_score: begin
+                    check_detail <= 0;
+                    if (detail_score < detail_target) begin
+                        state <= s_increase_detail;
+                    end
+                    else if (detail_score > detail_target) begin
+                        state <= s_decrease_detail;
+                    end
+
+                    else if (detail_score == detail_target) begin
+                        state <= s_make_video;
+                    end
+                end
+
+                s_increase_detail: begin
+                    detail_prmt <= detail_prmt + 1'b1; 
+                    state <= s_make_detail;
+                end
+
+                s_decrease_detail: begin
+                    detail_prmt <= detail_prmt - 1'b1; 
+                    state <= s_make_detail;
+                end
+
+                s_make_video: begin
+                    make_video <= 1;
+                    state <= s_done;
+                end
+
+                s_done: begin
+                    make_video <= 0;
+                    if (rst) begin
+                        state <= s_reset;
+                    end
+                end
+            endcase
+        end
     end
 
 endmodule
--- assignment_1/tb_fsm.sv
+++ assignment_1/tb_fsm.sv
@@ -13,7 +13,7 @@ module tb_Fsm ();
     typedef enum {s_reset, s_make_motion, s_check_motion, s_wait_motion_sccore, s_increase_motion, s_decrease_motion, s_decode_detail, 
     s_make_detail, s_check_detail, s_wait_detail_score, s_increase_detail, s_decrease_detail, s_make_video, s_done} statetype;
     
-    Fsm DUT (rst, clk, aim, motion_score, detail_score, make_motion, check_motion, make_detail, check_detail, make_video, motion_prmt, detail_prmt); // Instantiate DUT. 
+    Fsm dut (rst, clk, aim, motion_score, detail_score, make_motion, check_motion, make_detail, check_detail, make_video, motion_prmt, detail_prmt); // Instantiate DUT. 
 
     initial begin // Genreate the clock signal. 
         clk = 0; #5;
@@ -25,41 +25,41 @@ module tb_Fsm ();
 
     initial begin
         rst = 1; #15;
-        assert (dut.current_state === s_reset) else $error ("reset failed."); // When rst is asserted, we should go to the reset state.
+        assert (dut.state === s_reset) else $error ("reset failed."); // When rst is asserted, we should go to the reset state.
         assert (make_motion === 1'b0 && check_motion === 1'b0 && make_detail === 1'b0 && check_detail === 1'b0 && make_video === 1'b0) else $error ("Enable signals reset failed."); // Check if all enable signals are 0. 
-        assert (motion_target === 'd100 && motion_prmt === 'd50 && detail_target === 'd0 && detail_prmt === 'd150) else $error ("Parameters and targets initialization failed."); // Check if the targets and parameters are initialized properly. 
+        assert (dut.motion_target === 'd100 && motion_prmt === 'd50 && dut.detail_target === 'd0 && detail_prmt === 'd150) else $error ("Parameters and targets initialization failed."); // Check if the targets and parameters are initialized properly. 
         rst = 0; #10; // De-assert the reset signal. 
-        assert (dut.current_state === s_make_motion && make_motion === 1'b1) else $error ("make_motion failed."); // As the clock evolves we should go to subsequent states according to the state transition diagram. 
+        assert (dut.state === s_make_motion && make_motion === 1'b1) else $error ("make_motion failed."); // As the clock evolves we should go to subsequent states according to the state transition diagram. 
         #10;
-        assert (dut.current_state === s_check_motion && make_motion === 1'b0 && check_motion === 1'b1) else $error ("check_motion failed.");  
+        assert (dut.state === s_check_motion && make_motion === 1'b0 && check_motion === 1'b1) else $error ("check_motion failed.");  
         #10;
-        assert (dut.current_state === s_wait_motion_sccore && check_motion === 1'b0) else $error ("wait_motion_score failed.");
+        assert (dut.state === s_wait_motion_sccore && check_motion === 1'b0) else $error ("wait_motion_score failed.");
         motion_score = 'd80; #10; // Provide motion_score to check whether the FSM does the comparision with the motion_target correctly or not. 
-        assert (dut.current_state === s_increase_motion && motion_prmt === 'd51) else $error ("increase_motion failed.");
+        assert (dut.state === s_increase_motion && motion_prmt === 'd51) else $error ("increase_motion failed.");
         #30;
         motion_score = 'd101; #10; 
-        assert (dut.current_state === s_decrease_motion && motion_prmt === 'd50) else $error ("decrease_motion failed.");
+        assert (dut.state === s_decrease_motion && motion_prmt === 'd50) else $error ("decrease_motion failed.");
         #30; 
         motion_score = 'd100; #10;
-        assert (dut.current_state === s_decode_detail && motion_prmt === 'd50) else $error ("decode_detail failed.");
+        assert (dut.state === s_decode_detail && motion_prmt === 'd50) else $error ("decode_detail failed.");
         aim = 3'b100; #10; // Provide aim to check if the FSM assign the correct detail_target. 
-        assert (dut.current_state === s_make_detail && dut.detail_target === 'd60 && make_detail === 1'b1) else $error ("make_detail_high failed.");
+        assert (dut.state === s_make_detail && dut.detail_target === 'd60 && make_detail === 1'b1) else $error ("make_detail_high failed.");
         #10; 
-        assert (dut.current_state === s_check_detail && make_detail === 1'b0 && check_detail === 1'b1) else $error ("check_detail failed.");
+        assert (dut.state === s_check_detail && make_detail === 1'b0 && check_detail === 1'b1) else $error ("check_detail failed.");
         #10; 
-        assert (dut.current_state === s_wait_detail_score && check_detail === 1'b0) else $error ("wait_detail_score failed.");
+        assert (dut.state === s_wait_detail_score && check_detail === 1'b0) else $error ("wait_detail_score failed.");
         detail_score = 'd59; #10; // Provide detail_score to check whether the FSM does the comparision with the detail_target correctly or not. 
-        assert (dut.current_state === s_increase_detail && detail_prmt === 'd151) else $error ("increase_detail failed.");
+        assert (dut.state === s_increase_detail && detail_prmt === 'd151) else $error ("increase_detail failed.");
         #30;
         detail_score = 'd61; #10; 
-        assert (dut.current_state === s_decrease_detail && detail_prmt === 'd150) else $error ("decrease_detail failed.");
+        assert (dut.state === s_decrease_detail && detail_prmt === 'd150) else $error ("decrease_detail failed.");
         #30;
         detail_score = 'd60; #10;
-        assert (dut.current_state === s_make_video && make_video === 1'b1) else $error ("make_video failed.");
+        assert (dut.state === s_make_video && make_video === 1'b1) else $error ("make_video failed.");
         #10; 
-        assert (dut.current_state === s_done && make_video === 1'b0) else $error ("done failed.");
+        assert (dut.state === s_done && make_video === 1'b0) else $error ("done failed.");
         rst = 1; #10; // Check the reset of the FSM when it is done. 
-        assert (dut.current_state === s_reset) else $error ("reset failed.");
+        assert (dut.state === s_reset) else $error ("reset failed.");
         $stop; 
     end
 
